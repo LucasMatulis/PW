59f6e5e2887ca23ca444ae18fd1e4dab57cefb18
Matulis <70170180+LucasMatulis@users.noreply.github.com>
2022-09-12 11:44:18 -0300

    a

---
+++ apptest/src/pages/Cadastro/index.js
@@ -0,0 +1,12 @@
+function Cadastro(){
+
+
+
+    return(
+        <div>
+            <h1>Pagina Cadastro</h1>
+        </div>
+    )
+}
+
+export default Cadastro;
--- apptest/src/routes.js
+++ apptest/src/routes.js
@@ -5,12 +5,15 @@ import { BrowserRouter,
         from "react-router-dom";
 //
 import Home from './pages/Home';
+import Cadastro from './pages/Cadastro';
 //criar componente de roteamento
 function RouterApp(){
     return(
     <BrowserRouter>
     <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/cadastro" element={<Cadastro/>}/>
+
     </Routes>
     </BrowserRouter>
     )
