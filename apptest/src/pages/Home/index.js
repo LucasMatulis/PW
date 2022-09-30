22ea936b2676dd90ae660fa9a2724dbb33ed0b26
Lana Ribeiro <107940315+Lana-Ribeiro@users.noreply.github.com>
2022-09-30 16:08:33 -0300

    final
    l

--- apptest/src/pages/Home/index.js
+++ apptest/src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import style from './stylehome.css';
+import Imgel from "./fund.png";
 
 function Home(){
 
@@ -10,6 +11,7 @@ function Home(){
             <h1 align='center'>Pagina Home</h1>
             <br></br>
             <h2>O Banco da vida de 20 milhões de pessoas</h2>
+            <img class="fund" src={Imgel} aling="center"/>
             
         </div>
     )
