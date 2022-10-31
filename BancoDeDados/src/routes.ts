20ad9d255ff05eb2850f14daefd4dba7d868f01d
Matulis <70170180+LucasMatulis@users.noreply.github.com>
2022-10-31 09:43:21 -0300

    api de banco de dados

---
+++ BancoDeDados/src/routes.ts
@@ -0,0 +1,9 @@
+import { Router, Request, Response } from "express";
+
+const router = Router();
+
+router.get('/teste', (req: Request, res: Response) => {
+    throw new Error("Erro ao fazer esta requisição")
+})
+export { router };
+
